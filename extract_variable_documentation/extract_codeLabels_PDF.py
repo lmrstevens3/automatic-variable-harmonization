59d0ca628e3cfbb289fafe49047cd65e81c313c6
Laura Stevens <lmrstevens3@gmail.com>
2020-06-27 16:06:31 -0600

    Created gitignore and set output folder(ignored)

--- extract_variable_documentation/extract_codeLabels_PDF.py
+++ extract_variable_documentation/extract_codeLabels_PDF.py
@@ -1,6 +1,6 @@
 import camelot
 
-testFile_pdf = 'C:/Users/pielk/PycharmProjects/automatic-variable-mapping/HF_Action_Data_Dictionary.pdf'
+testFile_pdf = 'HF_Action_Data_Dictionary.pdf'
 
 tables = camelot.read_pdf(testFile_pdf, pages='1', flavor='stream')
-tables.export('C:/Users/pielk/PycharmProjects/automatic-variable-mapping/foo.csv', f='csv', compress=False)
+tables.export('output/foo.csv', f='csv', compress=False)
