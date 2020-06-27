a4f964ec5341af00cd7c041898190588d7e5922e
tuh8888 <harrison.pielke-lombardo@cuanschutz.edu>
2020-06-27 03:52:35 -0600

    Read file in with dataset name using Camelot

--- extract_variable_documentation/extract_codeLabels_PDF.py
+++ extract_variable_documentation/extract_codeLabels_PDF.py
@@ -1,23 +1,6 @@
-import pandas as pd
-import urllib, urllib2
-from tabula import read_pdf
-
-import docx2txt
 import camelot
 
-tables = camelot.read_pdf(testFile_pdf)
-tables.export('foo.csv', f='csv', compress=True)
+testFile_pdf = 'C:/Users/pielk/PycharmProjects/automatic-variable-mapping/HF_Action_Data_Dictionary.pdf'
 
-testFile_rtf = 'extract_variable_documentation/HF_ACTION_Data_Dictionary.rtf'
-testFile_doc = '/Users/laurastevens/Dropbox/Graduate School/BioLINCC files/Framingham cohort/documentation/questionnaires/periodic clinic exam/exam 09/lex0_9.doc'
-testFile_pdf = '~/Dropbox/Graduate\ School/BioLINCC\ files/ACCORD/ACCORD Data Dictionary.pdf'
-
-#text = docx2txt.process(testFile_doc)
-
-#text_file = open("/Users/laurastevens/Dropbox/Graduate School/BioLINCC files/aric/Documentation/Databooks/Visit 1/DERIVE13.txt", "r")
-#lines = text_file.read()
-
-#testCodes = pd.read_table("/Users/laurastevens/Dropbox/Graduate School/BioLINCC files/aric/Documentation/Databooks/Visit 1/DERIVE13.txt" , sep='\r', skip_blank_lines = True)
-#varTest = testCodes.loc[testCodes.columns[0] == 'DRNKR01']
-
-test  = read_pdf(testFile_pdf)
+tables = camelot.read_pdf(testFile_pdf, pages='1', flavor='stream')
+tables.export('C:/Users/pielk/PycharmProjects/automatic-variable-mapping/foo.csv', f='csv', compress=False)
