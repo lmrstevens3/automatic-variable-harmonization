f70e93cdd719d3f5652e5b3f19d02526f0dd3ab3
Laura Stevens <lmrstevens3@gmail.com>
2020-06-27 16:46:35 -0600

    Made tests for tfidf and vocab_similarity classes and fns

---
+++ test/test_vocab_similarity.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+
+
+class TestVariableSimilarityCalculator(TestCase):
+    def test_calculate_similarity(self):
+        self.fail()
+
+    def test_init_cache(self):
+        self.fail()
+
+    def test_append_cache(self):
+        self.fail()
+
+    def test_finalize_cached_output(self):
+        self.fail()
+
+    def test_cache_sim_scores(self):
+        self.fail()
+
+    def test_score_variables(self):
+        self.fail()
+
+    def test_variable_similarity(self):
+        self.fail()
+
+    def test_filter_scores(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_merge_score_results(self):
+        self.fail()
+
+    def test_partition(self):
+        self.fail()
+
+    def test_vals_differ_in_col(self):
+        self.fail()
+
+    def test_vals_differ_in_all_cols(self):
+        self.fail()
+
+    def test_val_in_any_row_for_col(self):
+        self.fail()
+
+    def test_select_top_sims(self):
+        self.fail()
+
+    def test_select_top_sims_by_group(self):
+        self.fail()
