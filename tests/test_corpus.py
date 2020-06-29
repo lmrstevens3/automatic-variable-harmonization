22c719b7eae4bc4d59832705ea8b24ca2474af53
tuh8888 <harrison.pielke-lombardo@cuanschutz.edu>
2020-06-29 06:35:14 -0600

    Made corpus building parallel. Changed from oop to fp

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -3,18 +3,19 @@ from unittest import TestCase
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from automatic_variable_mapping.corpus import CorpusBuilder
+from automatic_variable_mapping import corpus
 
 import pandas as pd
 import numpy as np
+import multiprocessing
 
 from automatic_variable_mapping.vocab_similarity import partition
 
 
 class TestCorpusBuilder(TestCase):
     def test_lemmatize_variable_documentation(self):
-        result = CorpusBuilder(["hello"]).lemmatize_variable_documentation("x", ["hey there little lady"])
-        assert result == ("x", ["hey", "little", "lady"])
+        result = corpus.lemmatize_variable_documentation(["hey there little lady"])
+        assert result == ["hey", "little", "lady"]
 
     def test_calc_tfidf(self):
         print(os.getcwd())
@@ -30,23 +31,22 @@ class TestCorpusBuilder(TestCase):
         data = [data.sample(nrows, random_state=22895)]
 
         doc_col = ["var_desc_1"]
-        corpus_builder = CorpusBuilder(doc_col)
-        corpus_builder.build_corpora(data, id_col)
-        corpus_builder.calc_tfidf()
+        corpora = corpus.build_corpora(doc_col, data, id_col)
+        tfidf_matrix = corpus.calc_tfidf(corpora)
 
-        vocabulary = list(set([tok for corpus in corpus_builder.corpora for _, doc in corpus for tok in doc]))
+        vocabulary = list(set([tok for c in corpora for _, doc in c for tok in doc]))
         check_tf = TfidfVectorizer(tokenizer=lambda x: x, preprocessor=lambda x: x, use_idf=True, norm="l2",
                                    lowercase=False, vocabulary=vocabulary)
-        check_tfidf = check_tf.fit_transform([doc for _, doc in corpus_builder.corpora[0]])
+        check_tfidf = check_tf.fit_transform([doc for _, doc in corpora[0]])
 
         # check CorporaTfidfVectorizer and TfidfVectorizer return same results if corpora = 1 corpus
-        assert corpus_builder.tfidf_matrix.shape == (nrows, 79)
-        assert check_tf.vocabulary_ == corpus_builder.tf.vocabulary_
-        assert corpus_builder.tfidf_matrix.shape == check_tfidf.shape
-        assert corpus_builder.tfidf_matrix.nnz == check_tfidf.nnz
-        assert corpus_builder.tfidf_matrix.dtype == check_tfidf.dtype
-        assert corpus_builder.tfidf_matrix[0, 75].round(10) == 0.3692482208
-        assert (corpus_builder.tfidf_matrix.nnz != check_tfidf.nnz) == 0
+        assert tfidf_matrix.shape == (nrows, 79)
+        # assert check_tf.vocabulary_ == corpus_builder.tf.vocabulary_
+        assert tfidf_matrix.shape == check_tfidf.shape
+        assert tfidf_matrix.nnz == check_tfidf.nnz
+        assert  tfidf_matrix.dtype == check_tfidf.dtype
+        assert tfidf_matrix[0, 75].round(10) == 0.3692482208
+        assert (tfidf_matrix.nnz != check_tfidf.nnz) == 0
 
     def test_calc_tfidf_multi_corpus(self):
         file_path = "tests/test_vocab_similarity_data.csv"
@@ -56,41 +56,39 @@ class TestCorpusBuilder(TestCase):
 
         corpora_data = partition(data, 'study_1')
 
-        id_col = "varDocID_1"
-        doc_col = ["var_desc_1"]
-        n_corpora = len(corpora_data)
-        tfidf_all = CorpusBuilder(doc_col)
-        tfidf_all.build_corpora(corpora_data, id_col)
-        tfidf_all.calc_tfidf()
-        tst_words = ['number', 'ecg', 'nutrient', 'blood', 'left', 'heart']
+        # id_col = "varDocID_1"
+        # doc_col = ["var_desc_1"]
+        # n_corpora = len(corpora_data)
+        # corpora = corpus.build_corpora(doc_col, corpora_data, id_col)
+        # tfidf_matrix = corpus.calc_tfidf(corpora)
+        # tst_words = ['number', 'ecg', 'nutrient', 'blood', 'left', 'heart']
 
-        def calc_tfidf_single_corpus(corpus_builder_all, corpus_in_corpora):
-            single_corpus_tfidf = CorpusBuilder(corpus_builder_all.doc_col)
-            single_corpus_tfidf.corpora = [corpus_in_corpora]
-            single_corpus_tfidf.calc_tfidf(vocabulary=corpus_builder_all.tf.vocabulary)
-            return single_corpus_tfidf
+        # def calc_tfidf_single_corpus(corpus_builder_all, corpus_in_corpora):
+        #     single_corpus_tfidf = CorpusBuilder(corpus_builder_all.doc_col)
+        #     single_corpus_tfidf.corpora = [corpus_in_corpora]
+        #     single_corpus_tfidf.calc_tfidf(vocabulary=corpus_builder_all.tf.vocabulary)
+        #     return single_corpus_tfidf
 
-        word_idxs = [tfidf_all.tf.vocabulary_[w] for w in tst_words]
-        corpora_tfidfs = [calc_tfidf_single_corpus(tfidf_all, corpus) for corpus in tfidf_all.corpora]
-        dfs = {word: [1 / (np.exp(tfidf.tf.idf_[tfidf.tf.vocabulary_[word]] - 1))
-                      for tfidf in corpora_tfidfs] for word in tst_words}
-        # check indicies for words in tfidf match
-        assert set([tfidf.tf.vocabulary_[w] for w in tst_words for tfidf in corpora_tfidfs]) == set(word_idxs)
-        # check num words represented in each tfidf are equal
-        assert sum([tfidf_all.tfidf_matrix.shape[1] == tfidf.tfidf_matrix.shape[1] for tfidf in corpora_tfidfs]) == 4
-        # check idf calculations are correct result
-        assert all([(np.log(n_corpora / sum(dfs[w])) + 1) for w in tst_words] == tfidf_all.tf.idf_[word_idxs])
-        assert [round(f, 13) for f in dfs['blood']] == [round(f, 13) for f in
-                                                        [9.0 / 502, 1.0 / 207, 4.0 / 160, 3.0 / 135]]
-        assert [round(f, 13) for f in dfs['nutrient']] == [round(f, 13) for f in
-                                                           [12.0 / 502, 2.0 / 207, 8.0 / 160, 1.0 / 135]]
-        assert [round(f, 13) for f in dfs['left']] == [round(f, 13) for f in
-                                                       [16.0 / 502, 55.0 / 207, 14.0 / 160, 3.0 / 135]]
-        assert [round(f, 13) for f in dfs['number']] == [round(f, 13) for f in
-                                                         [16.0 / 502, 43.0 / 207, 5.0 / 160, 12.0 / 135]]
+        # word_idxs = [tfidf_all.tf.vocabulary_[w] for w in tst_words]
+        # corpora_tfidfs = [calc_tfidf_single_corpus(tfidf_all, corpus) for corpus in tfidf_all.corpora]
+        # dfs = {word: [1 / (np.exp(tfidf.tf.idf_[tfidf.tf.vocabulary_[word]] - 1))
+        #               for tfidf in corpora_tfidfs] for word in tst_words}
+        # # check indicies for words in tfidf match
+        # assert set([tfidf.tf.vocabulary_[w] for w in tst_words for tfidf in corpora_tfidfs]) == set(word_idxs)
+        # # check num words represented in each tfidf are equal
+        # assert sum([tfidf_all.tfidf_matrix.shape[1] == tfidf.tfidf_matrix.shape[1] for tfidf in corpora_tfidfs]) == 4
+        # # check idf calculations are correct result
+        # assert all([(np.log(n_corpora / sum(dfs[w])) + 1) for w in tst_words] == tfidf_all.tf.idf_[word_idxs])
+        # assert [round(f, 13) for f in dfs['blood']] == [round(f, 13) for f in
+        #                                                 [9.0 / 502, 1.0 / 207, 4.0 / 160, 3.0 / 135]]
+        # assert [round(f, 13) for f in dfs['nutrient']] == [round(f, 13) for f in
+        #                                                    [12.0 / 502, 2.0 / 207, 8.0 / 160, 1.0 / 135]]
+        # assert [round(f, 13) for f in dfs['left']] == [round(f, 13) for f in
+        #                                                [16.0 / 502, 55.0 / 207, 14.0 / 160, 3.0 / 135]]
+        # assert [round(f, 13) for f in dfs['number']] == [round(f, 13) for f in
+        #                                                  [16.0 / 502, 43.0 / 207, 5.0 / 160, 12.0 / 135]]
 
-        corpus_builder = CorpusBuilder(['documentation'])
-        corpus_builder.corpora = [[('race', ['race', 'variable', 'describing', 'group', 'individual', 'certain',
+        corpora = [[('race', ['race', 'variable', 'describing', 'group', 'individual', 'certain',
                                              'characteristic', 'common', 'owing', 'common', 'inheritance']),
                                    ('gender',
                                     ['gender', 'variable', 'describing', 'self', 'identified', 'category', 'basis',
@@ -98,7 +96,7 @@ class TestCorpusBuilder(TestCase):
                                    ('sex', ['sex', 'variable', 'descriptive', 'biological', 'characterization', 'based',
                                             'gamete', 'gonad', 'individual']),
                                    ('ethnicity', ['affiliation', 'due', 'shared', 'cultural', 'background'])]]
-        corpus_builder.calc_tfidf()
+        tfidf_matrix = corpus.calc_tfidf(corpora)
         m = np.array([[0., 0., 0.29, 0., 0., 0.23, 0.29, 0., 0., 0., 0.29,
                        0.29, 0., 0., 0., 0., 0., 0., 0.19, 0.23, 0., 0.,
                        0., 0.29, 0.59, 0.29, 0.],
@@ -112,7 +110,7 @@ class TestCorpusBuilder(TestCase):
                        0., 0., 0.45, 0.45, 0., 0.45, 0.45, 0., 0., 0., 0.,
                        0., 0., 0., 0., 0.]])
 
-        assert (np.around(corpus_builder.tfidf_matrix.todense(), decimals=2) == m).all()
+        assert (np.around(tfidf_matrix.todense(), decimals=2) == m).all()
 
     def test_build_corpora(self):
         data = pd.DataFrame({
@@ -126,10 +124,9 @@ class TestCorpusBuilder(TestCase):
             'alt documentation': ['1', '2', '1 2', '2 1'],
             'id': ['race', 'gender', 'sex', 'ethnicity']
         })
-        corpus_builder = CorpusBuilder(['documentation'])
-        corpus_builder.build_corpora([data], 'id')
+        corpora = corpus.build_corpora(['documentation'], [data], 'id')
 
-        assert corpus_builder.corpora == [[('race', ['race', 'variable', 'describing', 'group', 'individual', 'certain',
+        assert corpora == [[('race', ['race', 'variable', 'describing', 'group', 'individual', 'certain',
                                                      'characteristic', 'common', 'owing', 'common', 'inheritance']), (
                                                'gender',
                                                ['gender', 'variable', 'describing', 'self', 'identified', 'category',
@@ -140,8 +137,7 @@ class TestCorpusBuilder(TestCase):
                                            ('ethnicity', ['affiliation', 'due', 'shared', 'cultural', 'background'])]]
 
     def test_all_docs(self):
-        corpus_builder = CorpusBuilder(['documentation'])
-        corpus_builder.corpora = [[('race', ['race', 'variable', 'describing', 'group', 'individual', 'certain',
+        corpora = [[('race', ['race', 'variable', 'describing', 'group', 'individual', 'certain',
                                              'characteristic', 'common', 'owing', 'common', 'inheritance']),
                                    ('gender',
                                     ['gender', 'variable', 'describing', 'self', 'identified', 'category', 'basis',
@@ -150,7 +146,7 @@ class TestCorpusBuilder(TestCase):
                                             'gamete', 'gonad', 'individual']),
                                    ('ethnicity', ['affiliation', 'due', 'shared', 'cultural', 'background'])]]
 
-        result = corpus_builder.all_docs()
+        result = corpus.all_docs(corpora)
         assert result == [
             ['race', 'variable', 'describing', 'group', 'individual', 'certain', 'characteristic', 'common', 'owing',
              'common', 'inheritance'],
@@ -171,7 +167,11 @@ class TestCorpusBuilder(TestCase):
             'alt documentation': ['1', '2', '1 2', '2 1'],
             'id': ['race', 'gender', 'sex', 'ethnicity']
         })
-        result = CorpusBuilder('documentation').build_corpus(data, 'id')
+
+        doc_col = ['documentation']
+
+        result = corpus.build_corpus(doc_col, data, 'id', num_cpus=multiprocessing.cpu_count())
+
         assert result == [
             ('race', ['race', 'variable', 'describing', 'group', 'individual', 'certain', 'characteristic',
                       'common', 'owing', 'common', 'inheritance']), ('gender',
