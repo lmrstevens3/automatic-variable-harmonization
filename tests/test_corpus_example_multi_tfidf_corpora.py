b701d953422c039e9d9721aff4a550dc80baaa33
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-04-10 13:58:28 -0600

    Start of a tfidf example by hand.

---
+++ tests/test_corpus_example_multi_tfidf_corpora.py
@@ -0,0 +1,14 @@
+corpora = [[['variable', 'describing', 'group', 'individual', 'certain', 'characteristic', 'common', 'owing', 'common', 'inheritance'],
+            ['variable', 'describing', 'self', 'identified', 'category', 'basis', 'sex'],
+            ['variable', 'descriptive', 'individual'],
+            [ 'background']],
+
+           [['race', 'describing', 'group', 'individual', 'certain', 'characteristic', 'common', 'owing', 'common', 'inheritance'],
+            ['gender', 'describing', 'self', 'identified', 'category', 'basis', 'sex'],
+            ['sex', 'descriptive', 'individual'],
+            ['affiliation', 'background']],
+
+           [['race', 'variable', 'group', 'individual', 'certain', 'characteristic', 'common', 'owing', 'common', 'inheritance'],
+            ['gender', 'variable', 'self', 'identified', 'category', 'basis', 'sex'],
+            ['sex', 'variable', 'individual'],
+            ['affiliation', 'background']]]
